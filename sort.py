82bc4e3a0fe078a52bd3975ca1e78937e77b55d6
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2022-11-21 21:16:55 +0200

    Commit 3
    Draft 3

--- sort.py
+++ sort.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import shutil
 
 
 def translate_name_file(name):
@@ -17,7 +18,7 @@ def replacement_symbols_file_name(name):
     list_name = list(name)
     i = 0
     for n in list_name:
-        if not i.isalpha():
+        if (not n.isalpha()) or (not n.isdigit()):
             list_name[i] = "_"
         i = i + 1
     new_name = "".join(list_name)
@@ -25,60 +26,52 @@ def replacement_symbols_file_name(name):
 
 
 def normalize(path):
-        full_file_name = path.name()
-        list_file_name = ".".split(full_file_name)
-        list_file_name[0] = translate_name_file(list_file_name[0])
-        list_file_name[0] = replacement_symbols_file_name(list_file_name[0])
-        new_file_name = ".".join(list_file_name)
-        new_path = path + "/" + new_file_name
-
-    return new_path
+    full_name = path.name()
+    list_name = ".".split(full_name)
+    list_name[0] = translate_name_file(list_name[0])
+    list_name[0] = replacement_symbols_file_name(list_name[0])
+    new_name = ".".join(list_name)
+    path.rename(new_name)
 
 
-def img_file(path):
+def delete(path):
+    try:
+        path.rmdir()
+    except OSError as e:
+        print(
+            f'Не вдалось видалити порожню папку за шляхом {path}. Помилка: {e.strerror}')
 
 
-def video_file(path):
-
-
-def document_file(path):
-
-
-def audio_file(path):
-
-
-def archive_file(path):
-
-
-def delete_file(path):
-
-
-def sorting(path):
+def sorting(path, video_path, archive_path, audio_path, document_path, image_path):
     unknown_list = []
     known_list = []
-    if len(p.iterdir()):
-        delete_file(p)
+    normalize(path)
+    if not len(p.iterdir()):
+        delete(p)
     for i in p.iterdir():
         if i.is_dir:
-            sorting(i)
+            sorting(i, video_path, archive_path,
+                    audio_path, document_path, image_path)
         else:
-            n_name = normalize(i)
-            p_name = n_name.name()
+            normalize(i)
+            p_name = i.name()
             list_name = p_name.split(".")
             if (list_name[1] == "jpg") or (list_name[1] == "png") or (list_name[1] == "jpeg") or (list_name[1] == "svg"):
-                img_file(n_name)
                 known_list.append(list_name[1])
+                shutil.move(i, image_path)
             elif (list_name[1] == "avi") or (list_name[1] == "mp4") or (list_name[1] == "mov") or (list_name[1] == "mkv"):
-                video_file(n_name)
+                shutil.move(i, video_path)
                 known_list.append(list_name[1])
             elif (list_name[1] == "doc") or (list_name[1] == "docx") or (list_name[1] == "txt") or (list_name[1] == "pdf") or (list_name[1] == "rtf") or (list_name[1] == "xslx") or (list_name[1] == "slx") or (list_name[1] == "pptx") or (list_name[1] == "ppt"):
-                document_file(n_name)
+                shutil.move(i, document_path)
                 known_list.append(list_name[1])
             elif (list_name[1] == "mp3") or (list_name[1] == "ogg") or (list_name[1] == "wav") or (list_name[1] == "amr"):
-                audio_file(n_name)
+                shutil.move(i, audio_path)
                 known_list.append(list_name[1])
             elif (list_name[1] == "zip") or (list_name[1] == "tar") or (list_name[1] == "gz"):
-                archive_file(n_name)
+                shutil.move(i, archive_path)
+                arh_p = i + "/" + list_name[0]
+                shutil.unpack_archive(arh_p)
                 known_list.append(list_name[1])
             else:
                 unknown_list.append(list_name[1])
@@ -88,4 +81,14 @@ p = Path(input("Введіть шлях до папки:"))
 if not p.is_dir:
     print("Це не шлях до папки!")
 else:
-    sorting(p)
+    video_path = p / '/video'
+    video_path.mkdir()
+    archive_path = p / "/archives"
+    archive_path.mkdir()
+    audio_path = p / "/audio"
+    audio_path.mkdir()
+    document_path = p / "/documents"
+    document_path.mkdir()
+    image_path = p / "/images"
+    image_path.mkdir()
+    sorting(p, video_path, archive_path, audio_path, document_path, image_path)
