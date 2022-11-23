8206ca6d83f1153b1ce72ee62356ac33de2db4a3
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2022-11-23 20:32:57 +0200

    Commit 23.11_1
    Version 1.1

--- sort.py
+++ sort.py
@@ -4,6 +4,8 @@ import os
 
 
 def translate_name_file(name) -> str:
+    """Функція транслітерації назв файлів та папок"""
+
     CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЄІЇҐ"
     TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
                    "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g", "A", "B", "V", "G", "D", "E", "E", "J", "Z", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U",
@@ -16,6 +18,8 @@ def translate_name_file(name) -> str:
 
 
 def replacement_symbols_file_name(name) -> str:
+    """Фунція заміни всіх небажаних символів в іменах файлів та папок"""
+
     list_name = list(name)
     i = 0
     for n in list_name:
@@ -27,6 +31,8 @@ def replacement_symbols_file_name(name) -> str:
 
 
 def normalize(path: Path) -> Path:
+    """Функція нормацізації імен файлів та папок"""
+
     if path.is_dir():
         full_name = path.name
         full_transtale_name = translate_name_file(full_name)
@@ -60,6 +66,8 @@ def normalize(path: Path) -> Path:
 
 
 def delete(path: Path):
+    """Функція видалення порожньої папки"""
+
     try:
         path.rmdir()
     except OSError as e:
@@ -74,10 +82,11 @@ def sorting(path: Path, video_path: Path, archive_path: Path, audio_path: Path, 
         delete(path)
         return []
     else:
-        for i in path.iterdir():
+        new_path_name = normalize(path)
+        for i in new_path_name.iterdir():
             if (i.name == "video") or (i.name == "audio") or (i.name == "archives") or (i.name == "documents") or (i.name == "images"):
                 continue
-            i_path = path / i
+            i_path = new_path_name / i
             if i_path.is_dir():
                 new_path = normalize(i_path)
                 l = sorting(new_path, video_path, archive_path,
@@ -89,27 +98,53 @@ def sorting(path: Path, video_path: Path, archive_path: Path, audio_path: Path, 
                 i_new = normalize(i_path)
                 #i_roz_with = i_new.name.split(".")
                 i_roz = i_new.suffix.removeprefix(".")
-                if (i_roz == "jpg") or (i_roz == "png") or (i_roz == "jpeg") or (i_roz == "svg"):
+                if (i_roz == "jpg") or (i_roz == "png") or (i_roz == "jpeg") or (i_roz == "svg") or (i_roz == "bmp"):
                     known_list.append(i_roz)
-                    shutil.move(i_new, image_path)
+                    try:
+                        shutil.move(i_new, image_path)
+                    except OSError as e:
+                        print(
+                            f"Не вдалося перемістити файл {i_new.name}. Помилка: {e.strerror}")
                 elif (i_roz == "avi") or (i_roz == "mp4") or (i_roz == "mov") or (i_roz == "mkv"):
-                    shutil.move(i_new, video_path)
                     known_list.append(i_roz)
+                    try:
+                        shutil.move(i_new, video_path)
+                    except OSError as e:
+                        print(
+                            f"Не вдалося перемістити файл {i_new.name}. Помилка: {e.strerror}")
                 elif (i_roz == "doc") or (i_roz == "docx") or (i_roz == "txt") or (i_roz == "pdf") or (i_roz == "rtf") or (i_roz == "xlsx") or (i_roz == "xls") or (i_roz == "pptx") or (i_roz == "ppt"):
-                    shutil.move(i_new, document_path)
                     known_list.append(i_roz)
+                    try:
+                        shutil.move(i_new, document_path)
+                    except OSError as e:
+                        print(
+                            f"Не вдалося перемістити файл {i_new.name}. Помилка: {e.strerror}")
                 elif (i_roz == "mp3") or (i_roz == "ogg") or (i_roz == "wav") or (i_roz == "amr"):
-                    shutil.move(i_new, audio_path)
                     known_list.append(i_roz)
-                elif (i_roz == "zip") or (i_roz == "tar") or (i_roz == "gz") or (i_roz == "rar"):
-                    shutil.move(i_new, archive_path)
-                    arh_p = archive_path / \
-                        i_new.name.removesuffix(i_new.suffix)
-                    shutil.unpack_archive(arh_p)
+                    try:
+                        shutil.move(i_new, audio_path)
+                    except OSError as e:
+                        print(
+                            f"Не вдалося перемістити файл {i_new.name}. Помилка: {e.strerror}")
+                elif (i_roz == "zip") or (i_roz == "tar") or (i_roz == "gz") or (i_roz == "rar") or (i_roz == "ZIP") or (i_roz == "7z"):
+                    try:
+                        shutil.move(i_new, archive_path)
+                    except OSError as e:
+                        print(
+                            f"Не вдалось перемістити файл {i_new.name}. Помилка: {e.strerror}")
+                    try:
+                        arh_p = archive_path / \
+                            i_new.name.removesuffix(i_new.suffix)
+                        shutil.unpack_archive(arh_p)
+                    except OSError as e:
+                        print(
+                            f'Не вдалось розпакуати архів {i_new.name}. Помилка: {e.strerror}')
                     known_list.append(i_roz)
                 else:
                     unknown_list.append(i_roz)
-        return [unknown_list, known_list]
+    if not len(os.listdir(path)):
+        delete(new_path_name)
+    return [unknown_list, known_list]
 
 
 p = Path(input("Введіть шлях до папки:"))
