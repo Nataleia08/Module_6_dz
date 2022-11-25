036b4490547cc265ea7c6a101d281b0107d01536
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2022-11-25 13:30:30 +0200

    Commit 25_11 #1
    Version 2.0

--- function.py
+++ function.py
@@ -76,6 +76,19 @@ def delete(path: Path):
             f'Не вдалось видалити порожню папку за шляхом {path}. Помилка: {e.strerror}')
 
 
+def move_files(path: Path, type_files: int, list_path: list):
+    """Функция сортування зображень"""
+    i_roz = path.suffix.removeprefix(".")
+    for i_path in list_path[1][type_files]:
+        if i_roz == i_path.lower():
+            try:
+                new_path = list_path[0][type_files] / i_path.lower()
+                move(path, new_path)
+            except OSError as e:
+                print(
+                    f"Не вдалося перемістити файл {path.name}. Помилка: {e.strerror}")
+
+
 def sorting(path: Path, list_path) -> list:
     """Функція сортування папок та файлів"""
 
@@ -98,52 +111,37 @@ def sorting(path: Path, list_path) -> list:
                     known_list.union(l[1])
             else:
                 i_new = normalize(i_path)
-                #i_roz_with = i_new.name.split(".")
+                # i_roz_with = i_new.name.split(".")
                 i_roz = i_new.suffix.removeprefix(".")
                 if (i_roz == "jpg") or (i_roz == "png") or (i_roz == "jpeg") or (i_roz == "svg") or (i_roz == "bmp"):
                     known_list.add(i_roz)
-                    try:
-                        move(i_new, list_path[0][4])
-                    except OSError as e:
-                        print(
-                            f"Не вдалося перемістити файл {i_new.name}. Помилка: {e.strerror}")
+                    move_files(i_new, 4, list_path)
                 elif (i_roz == "avi") or (i_roz == "mp4") or (i_roz == "mov") or (i_roz == "mkv"):
                     known_list.add(i_roz)
-                    try:
-                        move(i_new, list_path[0][0])
-                    except OSError as e:
-                        print(
-                            f"Не вдалося перемістити файл {i_new.name}. Помилка: {e.strerror}")
+                    move_files(i_new, 0, list_path)
                 elif (i_roz == "doc") or (i_roz == "docx") or (i_roz == "txt") or (i_roz == "pdf") or (i_roz == "rtf") or (i_roz == "xlsx") or (i_roz == "xls") or (i_roz == "pptx") or (i_roz == "ppt") or (i_roz == "vsdx"):
                     known_list.add(i_roz)
-                    try:
-                        move(i_new, list_path[0][3])
-                    except OSError as e:
-                        print(
-                            f"Не вдалося перемістити файл {i_new.name}. Помилка: {e.strerror}")
+                    move_files(i_new, 3, list_path)
                 elif (i_roz == "mp3") or (i_roz == "ogg") or (i_roz == "wav") or (i_roz == "amr"):
                     known_list.add(i_roz)
-                    try:
-                        move(i_new, list_path[0][2])
-                    except OSError as e:
-                        print(
-                            f"Не вдалося перемістити файл {i_new.name}. Помилка: {e.strerror}")
+                    move_files(i_new, 2, list_path)
                 elif (i_roz == "zip") or (i_roz == "tar") or (i_roz == "gz") or (i_roz == "rar") or (i_roz == "ZIP") or (i_roz == "7z"):
+                    known_list.add(i_roz)
+                    move_files(i_new, 1, list_path)
+                    arh_temp_path = i_new.name.removesuffix(i_new.suffix)
+                    arh_p = list_path[0][1] / arh_temp_path
                     try:
-                        move(i_new, list_path[0][1])
-                    except OSError as e:
-                        print(
-                            f"Не вдалось перемістити файл {i_new.name}. Помилка: {e.strerror}")
-                    try:
-                        arh_p = list_path[0][1] / \
-                            i_new.name.removesuffix(i_new.suffix)
                         unpack_archive(arh_p)
                     except OSError as e:
                         print(
                             f'Не вдалось розпакуати архів {i_new.name}. Помилка: {e.strerror}')
-                    known_list.add(i_roz)
                 else:
                     unknown_list.add(i_roz)
+                    try:
+                        move(i_new, list_path[0][5])
+                    except OSError as e:
+                        print(
+                            f"Не вдалось перемістити файл {i_new.name}. Помилка: {e.strerror}")
     if not len(listdir(path)):
         delete(new_path_name)
     return [unknown_list, known_list]
@@ -160,8 +158,8 @@ def creating_folder(path: Path) -> list:
         pod_video_path.mkdir(exist_ok=True)
     # ------Створення папок для архівів--------------
     archive_path = path / "archives"
-    zip_path = archive_path / 'ZIP'
-    list_arhive_path = ['RAR', '7Z', 'TAR', 'GZ']
+    archive_path.mkdir(exist_ok=True)
+    list_arhive_path = ['RAR', '7Z', 'TAR', 'GZ', 'ZIP']
     for name_path in list_arhive_path:
         pod_video_path = archive_path / name_path
         pod_video_path.mkdir(exist_ok=True)
@@ -183,11 +181,14 @@ def creating_folder(path: Path) -> list:
     image_path = path / "images"
     image_path.mkdir(exist_ok=True)
     list_images_path = ['JPEG', 'PNG', 'JPG', 'SVG']
-    for name_path in list_docum_path:
+    for name_path in list_images_path:
         pod_video_path = image_path / name_path
         pod_video_path.mkdir(exist_ok=True)
+    # ------Створення папки для іншого--------------
+    other_path = path / "other"
+    other_path.mkdir(exist_ok=True)
     main_list_dir = [video_path, archive_path,
-                     audio_path, document_path, image_path]
+                     audio_path, document_path, image_path, other_path]
     list_path = [list_video_path, list_video_path,
                  list_audio_path, list_docum_path, list_images_path]
     return [main_list_dir, list_path]
